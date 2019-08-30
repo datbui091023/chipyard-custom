meghaanniegeorge/chipyard

add tracegen project

variables.mk
@@ -38,6 +38,17 @@ ifeq ($(SUB_PROJECT),example)
 	TB                ?= TestDriver
 	TOP               ?= BoomRocketTop
 endif
+ifeq ($(SUB_PROJECT),tracegen)
+	SBT_PROJECT       ?= tracegen
+	MODEL             ?= TestHarness
+	VLOG_MODEL        ?= $(MODEL)
+	MODEL_PACKAGE     ?= $(SBT_PROJECT)
+	CONFIG            ?= NonBlockingTraceGenConfig
+	CONFIG_PACKAGE    ?= $(SBT_PROJECT)
+	GENERATOR_PACKAGE ?= $(SBT_PROJECT)
+	TB                ?= TestDriver
+	TOP               ?= GroundTestSubsystem
+endif
 # for BOOM developers
 ifeq ($(SUB_PROJECT),boom)
 	SBT_PROJECT       ?= boom

datbui091023/chipyard-custom

variables.mk
@@ -38,6 +38,17 @@ ifeq ($(SUB_PROJECT),example)
 	TB                ?= TestDriver
 	TOP               ?= BoomRocketTop
 endif
+ifeq ($(SUB_PROJECT),tracegen)
+	SBT_PROJECT       ?= tracegen
+	MODEL             ?= TestHarness
+	VLOG_MODEL        ?= $(MODEL)
+	MODEL_PACKAGE     ?= $(SBT_PROJECT)
+	CONFIG            ?= TraceGenConfig
+	CONFIG_PACKAGE    ?= $(SBT_PROJECT)
+	GENERATOR_PACKAGE ?= $(SBT_PROJECT)
+	TB                ?= TestDriver
+	TOP               ?= TraceGenSystem
+endif
 # for Rocket-chip developers
 ifeq ($(SUB_PROJECT),rocketchip)
 	SBT_PROJECT       ?= rocketchip